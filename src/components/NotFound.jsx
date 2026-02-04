d9ac60dba62f3be9a729c1fe6410fc2e3e3f9122
dasarisaiteja <dasarisaiteja210@gmail.com>
2026-02-04 19:20:28 +0530

    desinged page not found component

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -1,7 +1,11 @@
+import { useRouteError } from "react-router-dom"
 function NotFound(){
+    const err=useRouteError()
     return (
         <>
-
+            <h1>ERROR</h1>
+            <h2>{err.status}</h2>
+            <h2>{err.data}</h2>
         </>
     )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,20 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import {createBrowserRouter,RouterProvider} from "react-router-dom"
+import NotFound from "./components/NotFound.jsx"
 import App from './App.jsx'
+
+let appRouter = createBrowserRouter([
+  {
+    path:"/",
+    element:<App/>,
+    errorElement:<NotFound/>
+  }
+])
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={appRouter}/>
   </StrictMode>,
 )
