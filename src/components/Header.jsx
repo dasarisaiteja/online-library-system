18bb91fa75ad61892e04652623174407ae42cf23
dasarisaiteja <dasarisaiteja210@gmail.com>
2026-02-04 12:51:26 +0530

    created HOME component

---
+++ src/components/Header.jsx
@@ -0,0 +1,19 @@
+function Header(){
+    return (
+        <>
+        <ul>
+            <li>
+                HOME
+            </li>
+            <li>
+                Browse Books
+            </li>
+            <li>
+                Add Book
+            </li>
+        </ul>
+        </>
+    )
+}
+
+export default Header
